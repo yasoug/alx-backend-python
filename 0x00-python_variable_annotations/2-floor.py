a51e1816be83870e07c98fa3dd6bd7d18d3de64b
yasoug <ougouisyassine@gmail.com>
2024-01-11 19:32:38 +0000

    task 2 : commit 1

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Script for function floor"""
+
+
+def floor(n: float) -> int:
+    """
+    Type-annotated function floor,
+    takes a float as argument and returns the floor of the float
+    """
+    return int(n)
