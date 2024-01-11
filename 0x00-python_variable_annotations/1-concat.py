f5c92412dae09d7866a65a09c3e87dbdb64fcc14
yasoug <ougouisyassine@gmail.com>
2024-01-11 19:30:01 +0000

    task 1 : commit 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Script for function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Type-annotated function to concatenate two strings"""
+    return str1 + str2
