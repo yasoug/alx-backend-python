b35cbb1d43ef8fef5224bd8e984764c094e4e353
yasoug <ougouisyassine@gmail.com>
2024-01-11 19:34:34 +0000

    task 3 : commit 1

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Script for function to_str"""
+
+
+def to_str(n: float) -> str:
+    """
+    Type-annotated function to_str,
+    takes a float as argument and returns the string representation of it
+    """
+    return str(n)
