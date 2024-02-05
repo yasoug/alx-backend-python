cb896196528ba500cf7debf311911e786ac1a8d2
yasoug <ougouisyassine@gmail.com>
2024-02-05 19:49:34 +0000

    task 0 : commit 1

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Script for testing the utils module"""
+
+from unittest import TestCase
+from parameterized import parameterized
+from typing import Mapping, Sequence, Dict, Tuple, Union
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(TestCase):
+    """Test cases for the access_nested_map function"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(
+            self,
+            nested_map: Mapping,
+            path: Sequence,
+            expected: Union[Dict, int]
+            ) -> None:
+        """Test the output of access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
