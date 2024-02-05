e6420c87470c4be97752377925e7bbc65eae70c1
yasoug <ougouisyassine@gmail.com>
2024-02-05 20:18:25 +0000

    task 3 : commit 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 from unittest import TestCase
 from parameterized import parameterized
 from typing import Mapping, Sequence, Dict, Tuple, Union
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import Mock, patch
 
 
@@ -57,3 +57,28 @@ class TestGetJson(TestCase):
         with patch("requests.get", return_value=Mock(**attrs)) as req_get:
             self.assertEqual(get_json(test_url), test_payload)
             req_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(TestCase):
+    """Test cases for the memoize decorator"""
+    def test_memoize(self) -> None:
+        """Test memoize decorator by mocking a_method"""
+        class TestClass:
+            """Dummy class for testing"""
+            def a_method(self):
+                """Mocked method"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """Property under test using memoize decorator"""
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fct:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fct.assert_called_once()
